277803b7670ad056e86733fe12b234aa1e1eb9ce
andrea delrio <delrio.andre@gmail.com>
2013-08-19 11:41:02 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookme::Application.config.secret_key_base = '737a1975da50f228a828e3273008bffef5f0c976e885e4ebac1f09916f5eb0965008c338ae9518ac934623d1da4e71766e47372c389a9bea18d8ea8cd4fcb68c'
